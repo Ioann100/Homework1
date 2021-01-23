6d3fd5f948efe6e6b02dcfa83c3137b23231e151
Ioann100 <ivanmikolenko03@gmail.com>
2021-01-23 21:21:10 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,209 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+         Task1();
+       // Task2();
+        //Task3_1();
+        //Task3_2();
+        // Task3_3();
+    }
+
+
+    private static void Task1() {
+        System.out.println("⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⣠⣤⣤⣤⣤⣤⣀⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄");
+        System.out.println("⠄⠄⠄⠄⠄⠄⣀⣤⣤⣤⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⣀⠄⠄⠄⠄⠄⠄");
+        System.out.println("⠄⠄⠄⠄⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠄⠄⠄⠄");
+        System.out.println("⠄⠄⠄⠄⣾⣿⣿⣿⣿⠿⠛⠛⠛⠿⠿⠿⠋⠉⠙⠻⣿⣿⣿⣿⣿⣷⠄⠄⠄⠄");
+        System.out.println("⠄⠄⠄⢰⣿⣿⣿⣿⡇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠙⢿⣿⣿⣿⣇⠄⠄⠄");
+        System.out.println("⠄⠄⠄⢸⣿⣿⣿⣿⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢻⣿⣿⣿⡀⠄⠄");
+        System.out.println("⠄⠄⠄⣸⣿⣿⣿⣿⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⡇⠄⠄");
+        System.out.println("⠄⠄⠄⣿⣿⣿⣿⠛⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⡇⠄⠄");
+        System.out.println("⠄⠄⠄⣿⣿⣿⣿⣿⣿⣿⣷⣶⣤⣀⣀⣀⣴⣶⣶⣶⣶⣦⣤⣀⣿⣿⣿⡇⠄⠄");
+        System.out.println("⠄⠄⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠉⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⠄⠄");
+        System.out.println("⠄⠄⢸⣿⣿⣿⡿⣿⣿⣿⣿⠟⣿⠇⠄⠘⢿⣿⣿⣿⣿⣿⡿⠋⢹⡟⠡⡌⡇⠄");
+        System.out.println("⠄⠄⢸⣿⣿⣿⣧⠄⠈⠁⠄⢠⣿⠄⠄⠄⠄⠉⠙⠋⠉⠁⠄⠄⢸⢠⡄⠄⡇⠄");
+        System.out.println("⠄⠄⠸⣿⣿⣿⣿⣆⠄⠄⠄⠸⡟⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠉⢀⠃⠄");
+        System.out.println("⠄⠄⠄⠹⢿⣿⣿⣿⠆⠄⠄⠾⣿⡿⠿⠛⠂⠄⠄⠄⠄⠄⠄⠄⠄⣀⡠⠊⠄⠄");
+        System.out.println("⠄⠄⠄⠄⠄⣿⣿⡟⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⡏⠁⠄⠄⠄⠄");
+        System.out.println("⠄⠄⠄⠄⠄⠸⣿⡇⠄⠠⠖⠛⠋⠁⠉⠙⠒⠠⠄⠄⠄⠄⠄⢸⠁⠄⠄⠄⠄⠄");
+        System.out.println("⠄⠄⠄⠄⠄⠄⠹⣿⡀⠄⠄⠈⠛⠿⠛⠃⠄⠄⠄⠄⠄⠄⣠⡟⠄⠄⠄⠄⠄⠄");
+        System.out.println("⠄⠄⠄⠄⠄⠄⢰⡌⢷⣦⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⣴⣾⣿⣿⡆⠄⠄⠄⠄⠄");
+        System.out.println("⠄⠄⠄⠄⠄⠄⢨⣿⣦⣝⣻⣷⣶⣶⣤⣤⣤⣴⣾⣿⣿⣿⣿⣿⣇⠄⠄⠄⠄⠄");
+        System.out.println("⠄⠄⠄⠄⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠄⠄⠄⠄");
+        System.out.println("⠄⠄⠄⠄⠄⠄⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣄⠄⠄⠄");
+        System.out.println("⠄⠄⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣤");
+        System.out.println("⠄⠄⠄⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⠄⠄⠄⢀⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⣀⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+        System.out.println("⣿⠄⣿⣿⣿⠄⣿⠄⠄⠄⣿⠄⠄⠄⣿⠄⠄⠄⠄⣿⠄⠄⠄⣿⠄⠄⣿⠄⠄⣿");
+        System.out.println("⣿⠄⠄⣿⠄⠄⣿⠄⣿⠄⣿⣿⠄⣿⣿⠄⣿⣿⠄⣿⣿⠄⣿⣿⣿⠄⠄⠄⣿⣿");
+        System.out.println("⣿⠄⣿⠄⣿⠄⣿⠄⠄⠄⣿⣿⠄⣿⣿⠄⠄⠄⠄⣿⣿⠄⣿⣿⣿⣿⠄⣿⣿⣿");
+        System.out.println("⣿⠄⣿⣿⣿⠄⣿⠄⣿⠄⣿⣿⠄⣿⣿⠄⣿⠄⣿⣿⣿⠄⣿⣿⣿⠄⠄⠄⣿⣿");
+        System.out.println("⣿⠄⣿⣿⣿⠄⣿⠄⣿⠄⣿⣿⠄⣿⣿⠄⣿⠄⣿⣿⠄⠄⠄⣿⠄⠄⣿⠄⠄⣿");
+        System.out.println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+
+        System.out.println("⠄⠄⠄⠄⠄⠄⣀⣠⣤⣤⣤⣤⣤⣤⣄⡀⠄⠄⠄⠄⠄⠄⠄⠄\n" +
+                "⠄⠄⠄⠄⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⠄⠄⠄⠄⠄⠄⠄\n" +
+                "⠄⠄⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢷⣆⠄⠄⠄⠄⠄\n" +
+                "⠄⣰⣿⣿⣿⣿⣿⠛⠛⠉⠄⠄⠄⠄⠄⠁⠄⠄⠄⠰⣶⣄⠄⠄\n" +
+                "⢸⣿⣿⣿⣿⣿⣿⡄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠘⣿⣷⠄\n" +
+                "⢸⣿⣿⣿⣿⣿⣿⡇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸⣿⡇\n" +
+                "⠸⣿⣿⣿⣿⣿⣿⣧⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸⣿⡇\n" +
+                "⠄⠹⣿⣿⣿⣿⣿⣿⣇⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸⣿⡇\n" +
+                "⢰⣦⣼⣿⣿⣿⣿⣿⣿⣿⣿⣷⡤⢠⣤⣶⣶⣶⣶⣶⣤⣼⡿⠄\n" +
+                "⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠄⠹⣿⣿⣿⣿⣿⣿⣿⠁⠄\n" +
+                "⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄⠄⠉⠛⠿⠿⠿⠿⠻⠄⠄\n" +
+                "⠄⠘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠄⠄⠄⠄⠄⠄⠄⠄⠂⠄⠄\n" +
+                "⠄⠄⠄⠹⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠛⠄⠄⠄⠄⠄⠄⠄⠄⠄\n" +
+                "⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⣀⣀⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄\n" +
+                "⠄⠄⠄⣠⣼⣿⣿⣿⣿⣿⣿⣟⣉⠉⠉⠙⠂⠄⠄⠄⠄⠄⠄⠄\n" +
+                "⠄⠄⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⠶⠄⠄⢀⠄⠄⠄⠄⠄\n" +
+                "⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣤⣀⠄⠄⠄⠄⣷⣄⠄⠄⠄\n" +
+                "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠁⠄⠄⠄⢻⣿⣷⣶⣤\n" +
+                "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠛⠉⠄⠄⠄⠄⠄⢸⣿⣿⣿⣿\n" +
+                "⣿⣿⣿⣿⣿⣿⣿⡟⠛⠁⠄⠄⠄⣴⣿⣆⠄⠄⠄⠸⣿⣿⣿⣿\n" +
+                "⣿⣿⣿⣿⣿⣿⣿⣧⠄⠄⠄⠄⢠⣿⣿⣿⡆⠄⠄⠄⣿⣿⣿⣿\n" +
+                "⣿⣿⣿⣿⣿⣿⣿⣿⣇⠄⠄⠄⢼⣿⣿⡇⠙⠄⠄⠄⢸⣿⣿⣿\n" +
+                "⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠄⠄⢨⣿⣿⣧⡀⠄⠄⠄⠄⣿⣿⣿\n" +
+                "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠄⢸⣿⣿⣿⣷⡄⠄⠄⠄⠸⣿⣿\n" +
+                "⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠇⠄⠸⠿⠿⠿⠿⠿⠆⠄⠄⠄⠿⠿");
+    }
+
+    private static void Task2() {
+        //	a = 3х + 2 -7
+        System.out.println("\n a=3х+2-7");
+        System.out.println("Введите x");
+        Scanner sc1  = new Scanner(System.in);
+        int x = sc1.nextInt();
+        int a = 3 * x + 2 - 7;
+        System.out.println(" х = " + x);
+        System.out.println(" a = " + a);
+
+        //  b = 4х*3 - 12
+        System.out.println("\n b=4х*3-12");
+        System.out.println("Введите x");
+        Scanner sc2 = new Scanner(System.in);
+        int x1 = sc2.nextInt();
+        int b = 4 * x1 * 3 - 12;
+        System.out.println(" x = " + x1);
+        System.out.println(" b = " + b);
+
+        //  c = х*(-3 + 2) + 1
+        System.out.println("\n c=х*(-3+2)+1");
+        Scanner sc3 = new Scanner(System.in);
+        int x2 = sc3.nextInt();
+        int c = x2 * (-3 + 2) + 1;
+        System.out.println(" x = " + x2);
+        System.out.println(" c = " + c);
+
+        //  d = (х - 2)*122
+        System.out.println("\n d =(x-2)*122");
+        Scanner sc4 = new Scanner(System.in);
+        int x3 = sc4.nextInt();
+        int d = (x - 2) * 122;
+        System.out.println(" x = " + x3);
+        System.out.println(" d = " + d);
+
+        // e = (х + 4)*(485 - 179)
+        System.out.println("\n e=(х+4)*(485-179)");
+        Scanner sc5 = new Scanner(System.in);
+        int x4 = sc5.nextInt();
+        int e = (x4 + 4) * (485 - 179);
+        System.out.println(" x = " + x4);
+        System.out.println(" e = " + e);
+
+        //f = 3 + 9х - 5/х - 1
+        System.out.println("\n f=3+9х-5/х-1");
+        Scanner sc6 = new Scanner(System.in);
+        float x5 = sc6.nextFloat(); ;
+        float f = 3 + 9 * x5 - 5 / x5 - 1;
+        System.out.println(" x = " + x5);
+        System.out.println(" f = " + f);
+
+        //g = 2,5*(4 - 6х) -5
+        System.out.println("\n g=2,5*(4-6х)-5");
+        Scanner sc7 = new Scanner(System.in);
+        double x6 = sc7.nextDouble();
+        double g = 2.5 * (4 - 6 * x6) - 5;
+        System.out.println(" x = " + x6);
+        System.out.println(" g = " + g);
+
+        //h = x/2 + 2x/4 + 3x/6
+        System.out.println("\n h=x/2+2x/4+3x/6");
+        Scanner sc8 = new Scanner(System.in);
+        float x7 = sc8.nextFloat();
+        float h = x7 / 2 + 2 * x7 / 4 + 3 * x7 / 6;
+        System.out.println(" x = " + x7);
+        System.out.println(" h = " + h);
+
+        //y = 5x - 3/x + 6 + 7x - 12
+        System.out.println("\n y=5x-3/x+6+7x-12");
+        Scanner sc9 = new Scanner(System.in);
+        float x8 = sc9.nextFloat();
+        float y = 5 * x8 - 3 / x8 + 6 + 7 * x8 - 12;
+        System.out.println(" x = " + x8);
+        System.out.println(" y = " + y);
+    }
+
+    private static void Task3_1() {
+        System.out.println("Подсчёт числа в указанной степени");
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Введите число");
+        int firstValue = scanner1.nextInt();
+        System.out.println("Введите степень");
+        Scanner scanner2 = new Scanner(System.in);
+        int secondValue = scanner2.nextInt();
+        double pow = Math.pow(firstValue, secondValue);
+        System.out.println("Ответ");
+        System.out.println(pow);
+
+
+    }
+
+    private static void Task3_2() {
+        System.out.println("Извлечение квадратного корня из числа");
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Введите число");
+        int number = scanner1.nextInt();
+        double SquareRoot = Math.sqrt(number);
+        System.out.println("Ответ");
+        System.out.println(SquareRoot);
+
+    }
+
+    private static void Task3_3() {
+        System.out.println("Решение квадратного уравнения");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите коэффициент a");
+        double a = scanner.nextDouble();
+        System.out.println("Введите коэффициент b");
+        double b = scanner.nextDouble();
+        System.out.println("Введите коэффициент c");
+        double c = scanner.nextDouble();
+        double d = b * b - 4 * a * c;
+        System.out.println("D = " + d);
+
+        if (d > 0) {
+            double x1 = (-b + Math.sqrt(d)) / (2 * a);
+            double x2 = (-b - Math.sqrt(d)) / (2 * a);
+            System.out.println("x1 = " + x1);
+            System.out.println("x2 = " + x2);
+        }
+
+        if (d == 0){
+            double x = -b / (2 * a);
+            System.out.println("x = " + x);
+        }
+
+        if (d < 0) {
+           System.out.println("Уравнение не имеет корней");
+           }
+
+    }
+}
+
